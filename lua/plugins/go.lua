57fdc8113c889d14a0adb0e09071a0fc1696b33e
Mats Faugli <mats.faugli@gmail.com>
2023-07-17 14:44:13 +0200

    add go build tags also for neotest

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,5 +1,5 @@
 return {
-  -- Override LSP settings for gopls
+  -- Override LSP settings
   -- See https://github.com/golang/tools/blob/master/gopls/doc/settings.md
   {
     "neovim/nvim-lspconfig",
@@ -11,6 +11,18 @@ return {
               buildFlags = { "-tags", "development" },
             },
           },
+        },
+      },
+    },
+  },
+
+  -- Override neotest settings
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          args = { "-tags=development" },
         },
       },
     },
